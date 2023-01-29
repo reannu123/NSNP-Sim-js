c7ed1cd1d2d609062a9bcf09de4e26ff07845737
reannu123 <reannumon123@gmail.com>
2023-01-30 00:54:23 +0800

    Initial functioning Spiking Matrix generator

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// n = number of functions
+// m = number of variables
+// g = number of neurons
+// q = number of spiking vectors
+
 // Configuration Matrix
 let C = [[1, 1, 2]];
 
@@ -26,9 +31,25 @@ let syn = [
   [2, 1],
 ];
 
+// Helper Functions
+function computeCombination(n) {
+  let combination = 1;
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] != 0) {
+      combination *= n[i];
+    }
+  }
+  return combination;
+}
 function computePossible(C) {
   // Initialize active matrix with size of L
   // active is a matrix
+  // let n be a list with number of elements equal to the number of columns of F
+  let n = [];
+  for (let i = 0; i < L[0].length; i++) {
+    n.push(0);
+  }
+
   let active = [];
   for (let i = 0; i < L.length; i++) {
     active.push([]);
@@ -37,30 +58,25 @@ function computePossible(C) {
     }
   }
 
-  // Let n be a list with number of elements equal to the number of columns of L
-  let n = [];
-  for (let i = 0; i < L[0].length; i++) {
-    n.push(0);
-  }
-
   // For each neuron in the system
   for (let j = 0; j < L[0].length; j++) {
+    let count = 0;
     // For each function in the system
     for (let i = 0; i < L.length; i++) {
       // make sure the function is in the neuron
       if (L[i][j] == 1) {
         if (checkThreshold(C, i)) {
           active[i][j] = 1;
-          n[j] += 1;
+          count += 1;
         } else {
           active[i][j] = 0;
         }
       }
     }
+    n[j] = count;
   }
   return { n, active };
 }
-
 function checkThreshold(C, i) {
   // Check if i is in Threshold functions
   for (let j = 0; j < T.length; j++) {
@@ -86,4 +102,65 @@ function checkThreshold(C, i) {
   }
   return true;
 }
-console.log(computePossible(C));
+function getFunctions(m, active) {
+  let functions = [];
+  for (let i = 0; i < active.length; i++) {
+    if (active[i][m] == 1) {
+      functions.push(i);
+    }
+  }
+  return functions;
+}
+
+// Algorithm 1: Spiking Matrix
+function generateSM(C) {
+  let possible = computePossible(C);
+  let n = possible.n; // Number of functions
+  let active = possible.active;
+  let q = computeCombination(n); // Number of spiking vectors
+  // Initialize matrix spiking matrix with size of combination x n
+  let S = [];
+  for (let i = 0; i < q; i++) {
+    S.push([]);
+    for (let j = 0; j < F.length; j++) {
+      S[i].push(0);
+    }
+  }
+
+  // for each neuron m find the number of possible spiking vectors
+  for (let m = 0; m < L[0].length; m++) {
+    let functions = getFunctions(m, active);
+
+    // For each neuron, if the number of functions is 0, then set all the values of the spiking vector to 0
+    if (n[m] == 0) {
+      //for each element in functions, and for each row in S, set the value of S to 0
+      for (let j = 0; j < functions.length; j++) {
+        for (let k = 0; k < S.length; k++) {
+          S[k][functions[j]] = 0;
+        }
+      }
+    }
+    // If the number of functions is 1, then set all the values of the spiking vector to 1
+    else {
+      let i = 0;
+      let p = q / n[m];
+      for (let j = 0; j < functions.length; j++) {
+        let k = 1;
+        while (k <= p) {
+          S[i][functions[j]] = 1;
+          k++;
+          i++;
+        }
+      }
+    }
+    q = q / n[m];
+  }
+  S[1][2] = 1;
+  return S;
+}
+console.log("Configuration Matrix: ", C);
+console.log("Function Matrix: ", F);
+console.log("Function Location Matrix: ", L);
+console.log("Threshold List: ", T);
+console.log("Synapse List: ", syn);
+console.log("Spiking Matrix: ", generateSM(C));
